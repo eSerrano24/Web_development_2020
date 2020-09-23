a0a65fbf1ba527b87e626f224dd92d46c7e4cde5
Eric <beatjunkieeric@gmail.com>
2020-09-22 20:55:26 -0700

    create calculator app using node and express

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const { response } = require("express");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  response.send("index.html", { root: _dirname });
+  res.send("<h1>Hello!</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("Contact me @ fakeemail@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("I own this page");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("i like to code");
+});
 app.listen(3000, function () {
   console.log("server started on port 3000");
 });
